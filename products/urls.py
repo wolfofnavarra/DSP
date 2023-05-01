e08c0c987dd76f33882c484b6474dcf12343b099
wolfofnavarra <jfmocnavarro@gmail.com>
2023-05-01 15:29:09 -0500

    first commit

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import chart_select_view
+
+app_name = 'products'
+
+urlpatterns = [
+    path('', chart_select_view, name = 'main-products-view')
+]
+
+ 
---
+++ products/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from .models import Product, Purchase
+import pandas as pd
+
+# Create your views here.
+
+def chart_select_view(request):
+    error_message = None
+    df = None
+
+
+    product_df = pd.DataFrame(Product.objects.all().values())
+    purchase_df = pd.DataFrame(Purchase.objects.all().values())
+    product_df['product_id'] = product_df['id']
+    if purchase_df.shape[0] > 0:
+
+        df = pd.merge(purchase_df, product_df, on = 'product_id').drop(['id_y', 'date_y'], axis = 1).rename({'id_x': 'id', 'date_x': 'date'}, axis = 1)
+        if request.method == 'POST':
+            char_type = request.POST['sales']
+            date_from = request.POST['date_from']
+            date_to = request.POST['date_no']
+
+            df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
+
+            if char_type != "":
+                if date_from != "" and date_to != "":
+                    df = df[(df['date']>date_from) & (df['date'] < date_to)]
+                    df2 = df2.groupby('date', as_index = False)['total_price'].agg('sum')
+
+            else:
+                error_message = 'Please select a chart type to continue'
+    else:
+        error_message = "No records in the databse"
+
+
+    context = {
+        'error_message': error_message,
+        
+    }
+    return render(request, 'products/main.html', context)
